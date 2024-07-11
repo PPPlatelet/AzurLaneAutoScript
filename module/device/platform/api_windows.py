86a49bc92a56e7f710f2c4b8790e8eae0d63e405
PPPlatelet <ppplatelet@163.com>
2024-07-11 19:46:50 +0800

    Upd: fix.

--- module/device/platform/api_windows.py
+++ module/device/platform/api_windows.py
@@ -1,4 +1,5 @@
-import re
+import threading
+import asyncio
 
 from ctypes import byref, sizeof, create_unicode_buffer, wstring_at, addressof
 
@@ -8,6 +9,21 @@ from module.logger import logger
 
 
 def __yieldloop(entry32, snapshot, func: callable):
+    """
+    Generates a loop that yields entries from a snapshot until the function fails or finishes.
+
+    Args:
+        entry32 (PROCESSENTRY32 or THREADENTRY32): Entry structure to be yielded, either for processes or threads.
+        snapshot (int): Handle to the snapshot.
+        func (callable): Next entry (e.g., Process32Next or Thread32Next).
+
+    Yields:
+        PROCESSENTRY32 or THREADENTRY32: The current entry in the snapshot.
+
+    Raises:
+        OSError if any winapi failed.
+        IterationFinished if enumeration completed.
+    """
     while 1:
         yield entry32
         if func(snapshot, byref(entry32)):
@@ -24,8 +40,11 @@ def _enum_processes():
     Enumerates all the processes currently running on the system.
 
     Yields:
-        lppe32 (PROCESSENTRY32) |
-        None (if enum failed)
+        PROCESSENTRY32 or None: The current process entry or None if enumeration failed.
+
+    Raises:
+        OSError if CreateToolhelp32Snapshot or any winapi failed.
+        IterationFinished if enumeration completed.
     """
     lppe32          = PROCESSENTRY32()
     lppe32.dwSize   = sizeof(PROCESSENTRY32)
@@ -40,8 +59,11 @@ def _enum_threads():
     Enumerates all the threads currintly running on the system.
 
     Yields:
-        lpte32 (THREADENTRY32) |
-        None (if enum failed)
+        THREADENTRY32 or None: The current thread entry or None if enumeration failed.
+
+    Raises:
+        OSError if CreateToolhelp32Snapshot or any winapi failed.
+        IterationFinished if enumeration completed.
     """
     lpte32          = THREADENTRY32()
     lpte32.dwSize   = sizeof(THREADENTRY32)
@@ -51,13 +73,63 @@ def _enum_threads():
         yield from __yieldloop(lpte32, snapshot, Thread32Next)
 
 
+def _enum_events(hevent):
+    event = EVT_HANDLE()
+    returned = DWORD(0)
+    while EvtNext(hevent, 1, byref(event), INFINITE, 0, byref(returned)):
+        if event == INVALID_HANDLE_VALUE:
+            report(f"Invalid handle: 0x{event}", raiseexcept=False)
+            continue
+
+        buffer_size = DWORD(0)
+        buffer_used = DWORD(0)
+        property_count = DWORD(0)
+        rendered_content = None
+
+        EvtRender(
+            None,
+            event,
+            EVT_RENDER_EVENT_XML,
+            buffer_size,
+            rendered_content,
+            byref(buffer_used),
+            byref(property_count)
+        )
+        if GetLastError() == ERROR_SUCCESS:
+            yield rendered_content
+            continue
+
+        buffer_size = buffer_used.value
+        rendered_content = create_unicode_buffer(buffer_size)
+        if not rendered_content:
+            report("malloc failed.", raiseexcept=False)
+            continue
+
+        if not EvtRender(
+            None,
+            event,
+            EVT_RENDER_EVENT_XML,
+            buffer_size,
+            rendered_content,
+            byref(buffer_used),
+            byref(property_count)
+        ):
+            report(f"EvtRender failed with {GetLastError()}", raiseexcept=False)
+            continue
+
+        if GetLastError() == ERROR_SUCCESS:
+            yield rendered_content.value
+
+        EvtClose(event)
+
+
 def getfocusedwindow():
     """
     Get focused window.
 
     Returns:
         hwnd (int): Focused window hwnd
-        WINDOWPLACEMENT:
+        WINDOWPLACEMENT: The window placement or None if it couldn't be retrieved.
     """
     hwnd = GetForegroundWindow()
     if not hwnd:
@@ -90,22 +162,56 @@ def setforegroundwindow(focusedwindow: tuple = ()) -> bool:
     return True
 
 
-def execute(command: str, sstart: bool = False):
+def flash_window(focusedwindow: tuple, max_attempts: int = 10, interval: float = 0.5):
+    from time import sleep
+    attempts = 0
+    failed = 0
+
+    while attempts < max_attempts:
+        currentwindow = getfocusedwindow()
+
+        if not (focusedwindow[0] and currentwindow[0]):
+            failed += 1
+            if failed >= max_attempts:
+                report("Flash window failed.")
+            sleep(interval)
+            continue
+
+        if focusedwindow[0] != currentwindow[0]:
+            logger.info(f"Current window is {currentwindow[0]}, flash back to {focusedwindow[0]}")
+            setforegroundwindow(focusedwindow)
+            attempts += 1
+            continue
+
+        attempts += 1
+        sleep(interval)
+
+
+def execute(command: str, silentstart: bool, start: bool):
     """
     Create a new process.
 
     Args:
         command (str): process's commandline
-        sstart (bool): process's windowplacement
+        silentstart (bool): process's windowplacement
+        start (bool): True if start emulator, False if not
         Example:
             '"E:\\Program Files\\Netease\\MuMu Player 12\\shell\\MuMuPlayer.exe" -v 1'
 
     Returns:
         process: tuple(processhandle, threadhandle, processid, mainthreadid),
         focusedwindow: tuple(hwnd, WINDOWPLACEMENT)
+
+    Raises:
+        EmulatorLaunchFailedError if CreateProcessW failed.
     """
     from shlex import split
     from os.path import dirname
+    focusedwindow               = getfocusedwindow()
+    if start:
+        focus_thread = threading.Thread(target=flash_window, args=(focusedwindow, ))
+        focus_thread.start()
+
     lpApplicationName           = split(command)[0]
     lpCommandLine               = command
     lpProcessAttributes         = None
@@ -113,7 +219,7 @@ def execute(command: str, sstart: bool = False):
     bInheritHandles             = False
     dwCreationFlags             = (
         CREATE_NO_WINDOW |
-        NORMAL_PRIORITY_CLASS |
+        IDLE_PRIORITY_CLASS |
         CREATE_NEW_PROCESS_GROUP |
         CREATE_DEFAULT_ERROR_MODE |
         CREATE_UNICODE_ENVIRONMENT
@@ -123,10 +229,11 @@ def execute(command: str, sstart: bool = False):
     lpStartupInfo               = STARTUPINFOW()
     lpStartupInfo.cb            = sizeof(STARTUPINFOW)
     lpStartupInfo.dwFlags       = STARTF_USESHOWWINDOW
-    lpStartupInfo.wShowWindow   = SW_HIDE if sstart else SW_MINIMIZE
+    if start:
+        lpStartupInfo.wShowWindow   = SW_HIDE if silentstart else SW_MINIMIZE
+    else:
+        lpStartupInfo.wShowWindow   = SW_HIDE
     lpProcessInformation        = PROCESS_INFORMATION()
-
-    focusedwindow               = getfocusedwindow()
 
     success                     = CreateProcessW(
         lpApplicationName,
@@ -143,7 +250,7 @@ def execute(command: str, sstart: bool = False):
 
     if not success:
         report("Failed to start emulator.", exception=EmulatorLaunchFailedError)
-    
+
     process = (
         lpProcessInformation.hProcess,
         lpProcessInformation.hThread,
@@ -159,6 +266,9 @@ def terminate_process(pid: int):
 
     Args:
         pid (int): Emulator's pid
+
+    Raises:
+        OSError if OpenProcess failed.
     """
     with open_process(PROCESS_TERMINATE, pid) as hProcess:
         if TerminateProcess(hProcess, 0) == 0:
@@ -175,6 +285,9 @@ def get_hwnds(pid: int) -> list:
 
     Returns:
         hwnds (list): Emulator's possible window hwnds
+
+    Raises:
+        HwndNotFoundError if EnumWindows failed.
     """
     hwnds = []
 
@@ -203,7 +316,7 @@ def get_cmdline(pid: int) -> str:
         pid (int): Emulator's pid
 
     Returns:
-        command line (str): process's command line
+        cmdline (str): process's command line
         Example:
             '"E:\\Program Files\\Netease\\MuMu Player 12\\shell\\MuMuPlayer.exe" -v 1'
     """
@@ -234,18 +347,22 @@ def get_cmdline(pid: int) -> str:
             cmdline = wstring_at(addressof(commandLine), len(commandLine))
     except OSError:
         return ''
-    return cmdline.replace(r"\\", "/").replace("\\", "/").replace('"', '"')
+    return fstr(cmdline)
 
 
 def kill_process_by_regex(regex: str) -> int:
     """
-        Kill processes with cmdline match the given regex.
+    Kill processes with cmdline match the given regex.
 
-        Args:
-            regex:
+    Args:
+        regex:
 
-        Returns:
-            int: Number of processes killed
+    Returns:
+        int: Number of processes killed
+
+    Raises:
+        OSError if any winapi failed.
+        IterationFinished if enumeration completed.
     """
     count = 0
 
@@ -264,7 +381,7 @@ def kill_process_by_regex(regex: str) -> int:
         return count
 
 
-def _get_thread_creation_time(tid):
+def _get_thread_creation_time(tid: int):
     """
     Get thread's creation time.
 
@@ -273,6 +390,9 @@ def _get_thread_creation_time(tid):
 
     Returns:
         threadstarttime (int): Thread's start time
+
+    Raises:
+        OSError if OpenThread failed.
     """
     with open_thread(THREAD_QUERY_INFORMATION, tid) as hThread:
         creationtime    = FILETIME()
@@ -296,8 +416,12 @@ def get_thread(pid: int):
     Args:
         pid (int): Emulator's pid
 
-    Returns
+    Returns:
         mainthreadid (int): Emulator's main thread id
+
+    Raises:
+        OSError if any winapi failed.
+        IterationFinished if enumeration completed.
     """
     mainthreadid    = 0
     minstarttime    = MAXULONGLONG
@@ -327,7 +451,7 @@ def _get_process(pid: int):
 
     Returns:
         tuple(processhandle, threadhandle, processid, mainthreadid) |
-        tuple(None, None, processid, mainthreadid) | (if enum_process() failed)
+        tuple(None, None, processid, mainthreadid)
     """
     tid = get_thread(pid)
     try:
@@ -356,6 +480,10 @@ def get_process(instance: EmulatorInstance):
         tuple(processhandle, threadhandle, processid, mainthreadid) |
         tuple(None, None, processid, mainthreadid) | (if enum_process() failed)
         None (if enum_process() failed)
+
+    Raises:
+        OSError if any winapi failed.
+        IterationFinished if enumeration completed.
     """
     processes = _enum_processes()
     for lppe32 in processes:
@@ -364,20 +492,29 @@ def get_process(instance: EmulatorInstance):
         if not instance.path in cmdline:
             continue
         if instance == Emulator.MuMuPlayer12:
-            match = re.search(r'\d+$', cmdline)
-            if match and int(match.group()) == instance.MuMuPlayer12_id:
-                processes.close()
-                return _get_process(pid)
+            match = re.search(r'-v\s*(\d+)', cmdline)
+            if not match:
+                continue
+            if int(match.group(1)) != instance.MuMuPlayer12_id:
+                continue
+            processes.close()
+            return _get_process(pid)
         elif instance == Emulator.LDPlayerFamily:
-            match = re.search(r'\d+$', cmdline)
-            if match and int(match.group()) == instance.LDPlayer_id:
-                processes.close()
-                return _get_process(pid)
+            match = re.search(r'index=\s*(\d+)', cmdline)
+            if not match:
+                continue
+            if int(match.group(1)) != instance.LDPlayer_id:
+                continue
+            processes.close()
+            return _get_process(pid)
         else:
-            matchstr = re.search(fr'\b{instance.name}$', cmdline)
-            if matchstr and matchstr.group() == instance.name:
-                processes.close()
-                return _get_process(pid)
+            matchname = re.search(fr'{instance.name}(\s+|$)', cmdline)
+            if not matchname:
+                continue
+            if matchname.group(0).strip() != instance.name:
+                continue
+            processes.close()
+            return _get_process(pid)
 
 
 def switch_window(hwnds: list, arg: int = SW_SHOWNORMAL):
@@ -402,3 +539,71 @@ def switch_window(hwnds: list, arg: int = SW_SHOWNORMAL):
             continue
         ShowWindow(hwnd, arg)
     return True
+
+class ProcessManager:
+    # TODO UNDER DEVELOPMENT!!!!!! DO NOT USE!!!!
+    def __init__(self, pid: int):
+        self.mainpid = pid
+        self.datas = []
+        self.pids = []
+        self.evttree = EventTree()
+        self.lock = threading.Lock()
+        self.loop = asyncio.get_event_loop()
+        self.loop.create_task(self.listener())
+
+    async def listener(self):
+        # TODO listening grab/kill event
+        while True:
+            event = await self.get_event()
+            if event == "grab":
+                await self.grab_pids()
+            elif event == "kill":
+                await self.kill_pids()
+            await asyncio.sleep(1)
+
+    async def get_event(self):
+        # TODO get event
+        await asyncio.sleep(1)
+        return "grab"
+
+    async def grab_pids(self, pid: int):
+        if not IsUserAnAdmin():
+            return
+        with evt_query() as hevent:
+            events = _enum_events(hevent)
+            for content in events:
+                data = self.evttree.parse_event(content)
+                with self.lock:
+                    self.datas.append(data)
+                if data.process_id == pid:
+                    break
+            self.datas = self.datas[::-1]
+            await self.filltree()
+
+    async def filltree(self):
+        self.evttree.root = Node(self.datas[0])
+        for data in self.datas[1::]:
+            evtiter = self.evttree.pre_traversal(self.evttree.root)
+            for node in evtiter:
+                if data != node.data:
+                    continue
+                cmdline = get_cmdline(data.process_id)
+                if data.process_name not in cmdline:
+                    continue
+                node.add_children(data)
+
+    async def kill_pids(self):
+        # TODO kill process by enumerating tree
+        with self.lock:
+            evtiter = self.evttree.post_traversal(self.evttree.root)
+            for node in evtiter:
+                terminate_process(node.data.process_id)
+            while self.pids:
+                pass
+
+    def start(self):
+        threading.Thread(target=self.loop.run_forever).start()
+
+if __name__ == '__main__':
+    p = 1234
+    PM = ProcessManager(1234)
