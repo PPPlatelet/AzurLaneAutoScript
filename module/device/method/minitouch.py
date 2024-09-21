7414bc97fee50661f94f032a55319e06ba3a35d6
PPPlatelet <ppplatelet@163.com>
2024-09-21 16:05:42 +0800

    Upd: opt exception

--- module/device/method/minitouch.py
+++ module/device/method/minitouch.py
@@ -15,7 +15,7 @@ from module.base.timer import Timer
 from module.base.utils import *
 from module.device.connection import Connection
 from module.device.method.utils import RETRY_TRIES, handle_adb_error, handle_unknown_host_service, retry_sleep
-from module.exception import RequestHumanTakeover, ScriptError
+from module.exception import RequestHumanTakeover, ScriptError, ALASBaseError
 from module.logger import logger
 
 
@@ -357,11 +357,11 @@ class CommandBuilder:
         return empty
 
 
-class MinitouchNotInstalledError(Exception):
+class MinitouchNotInstalledError(ALASBaseError):
     pass
 
 
-class MinitouchOccupiedError(Exception):
+class MinitouchOccupiedError(ALASBaseError):
     pass
 
 
